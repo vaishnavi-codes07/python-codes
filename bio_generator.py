2402e8fa02ee0a8dc64a6a4d43232faaeb84b646
vaishnavi-codes07 <vaishnavidhonnar2008@gmail.com>
2026-09-02 10:51:16 +0530

    Dynamic Profile Bio Generator Live..!
    Leveling up my string manipulation skills! Created a script that turns 
    user input into attractive, Formatted GitHub bio using Python 
    F-strings.

---
+++ bio_generator.py
@@ -0,0 +1,12 @@
+name=input("Enter your name:",)
+role=("What are you learning? (e.g. Python,Web Dev):",)
+hobby=("What is your favorite hobby?:",)
+
+line_1=f"Hello World! My name is {name}."
+line_2=f"I am currently on a journey learning {role}."
+line_3=f"Outside of coding, I absolutely love {hobby}."
+print("/----YOUR GENERATED GITHUB BIO----")
+print(line_1)
+print(line_2)
+print(line_3)
+print("-------------------------------------")
