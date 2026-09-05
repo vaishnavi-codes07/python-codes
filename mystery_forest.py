5d80b32e2eb6721a05cacce82c26f705e5f3adf3
vaishnavi-codes07 <vaishnavidhonnar2008@gmail.com>
2026-09-05 11:13:52 +0530

    mystery doors logic
    Gold coins, a sleeping dragon, or an unexpected trip to space. 
    A terminal-based narrative experience.

---
+++ mystery_forest.py
@@ -0,0 +1,7 @@
+print("-----WELCOME TO THE MYSTERY FOREST-----")
+print("you are standing in front of two magical doors: 'LEFT' or 'RIGHT': ")
+choice=input("What do you do?")
+if choice=="LEFT":
+   print("WOW! You found a room filled with floating golden coins. You win!")
+if choice=="RIGHT":
+    print("Oh no!A sleeping dragon wakes up and chases you out. Game over!")
