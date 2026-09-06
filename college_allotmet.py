192819b1cd7e8602f624864a7830f4cc7f9bfcc9
vaishnavi-codes07 <vaishnavidhonnar2008@gmail.com>
2026-09-06 20:01:48 +0530

    coded my official college allotment letter.
    "Manifesting my engineering career through code. A simple yet impactful Python script mapping my provisional allotment details 
    for Bharati Vidyapeeth's AIML branch, proving that big goals start with clean, foundational lines of logic"

---
+++ college_allotmet.py
@@ -0,0 +1,7 @@
+college="Bharati Vidyapeeth College of women's,Katraj, pune."
+branch="Artificial Intelligence and Machine Learning."
+code="0628592150F"
+
+print("Institute alloted:" ,college)
+print("Course alloted:" ,branch)
+print("Choice code:" ,code)
