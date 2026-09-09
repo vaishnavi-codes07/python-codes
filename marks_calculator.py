22691bdca03b346a4ab304c0e333aec782924d0e
vaishnavi-codes07 <vaishnavidhonnar2008@gmail.com>
2026-09-09 19:23:11 +0530

    Python marks calculator
    A simple python script that calculates total marks, percentages, and 
    grades based on user input for various subjects.

---
+++ marks_calculator.py
@@ -0,0 +1,9 @@
+physics=45
+chemistry=45
+math=90
+total=physics+chemistry+math
+percent=(total/200)*100
+print("-------Marks Calculator-------")
+input("Enter your name:")
+print("Total marks are", total)
+print("Total percentage",percent)
