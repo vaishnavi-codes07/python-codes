b4cb31df4c8c9e26bbb9483889679e900c5f4208
vaishnavi-codes07 <vaishnavidhonnar2008@gmail.com>
2026-09-11 21:36:51 +0530

    Travel destination picker program
    This is simple python script created in my python journey.
    It takes user input to determine a travel destination and uses a conditional statements to recommend what packing items 
    to bring.

---
+++ destination_picker.py
@@ -0,0 +1,7 @@
+Destination=input("Where are you travelling?:")
+if Destination=="Beach":
+  print("Pack your swimsuit and sunglasses!")
+elif Destination=="Mountain":
+  print("Pack your warm jacket and hiking boots!")
+else:
+  print("Have a safe trip wherever you go!")
