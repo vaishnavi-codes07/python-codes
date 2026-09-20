a3e75dae81410537c5207b3bcea6fbf5489b3822
vaishnavi-codes07 <vaishnavidhonnar2008@gmail.com>
2026-09-20 16:24:18 +0530

    python dictionary data type demonstration script.
    A python script demonstrating how to create, access, and print data using Dictionary data type

---
+++ dictionary.py
@@ -0,0 +1,8 @@
+stdDetails1={"name": "Rajesh", "age": 25,"percentage": 86.2, "city":"Bangalore"}
+print("Data type of stdDetails is", type(stdDetails1))
+print("Students age is", stdDetails1["age"])
+print("Students percentage is", stdDetails1["percentage"])
+stdDetails1["rollNo"]=1001
+stdDetails1["age"]=26
+print("stdDetails1", stdDetails1)
+
