e7f8ca60de8795312222b062e5adfa9ed21b7a87
vaishnavi-codes07 <vaishnavidhonnar2008@gmail.com>
2026-09-20 15:51:35 +0530

    python data type project files.
    A simple python project demonstrating fundamentals built-in 
    data types and operations.

---
+++ datatypes.py
@@ -0,0 +1,7 @@
+num1=10.5
+print("Data type of num1 is", type(num1))
+num2=10+5j
+print("Data type of num2 is", type(num2))
+str1="Hello python.How are you?I am fine.Thank you"
+print(str1)
+print("Data type of str1 is", type(str1))
