465c15c5aff1ca71536ad708fce08eb7b668f1b0
vaishnavi-codes07 <vaishnavidhonnar2008@gmail.com>
2026-09-20 20:48:03 +0530

    python type casting and data structures conversions
    python script demonstrating explicit type casting and conversion 
    between strings, integers, floats, lists, tuples and sets.

---
+++ type_casting.py
@@ -0,0 +1,8 @@
+num1="100"
+num2=10.5
+print("Data type of num1 is", type(num1))
+print("Sum of num1 and num2 is", int(num1)+int(num2))
+str="My python code"
+print("Convert to list type:",list(str))
+print("Convert to tuple type:",tuple(str))
+print("Convert to set type:",set(str))
