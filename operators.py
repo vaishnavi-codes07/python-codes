583acabc1ef46802f75967b2baba332dea70d579
vaishnavi-codes07 <vaishnavidhonnar2008@gmail.com>
2026-09-21 19:45:14 +0530

    Python script demonstrating arithmetic operators
    A python script demonstrating foundational arithmetic operators ,etc., including basic math, floor division, exponents, and modulus.

---
+++ operators.py
@@ -0,0 +1,21 @@
+#Operators in python
+#Arithmetic Operators: +,-,*,/,%,**,//
+num1=7
+num2=2
+
+print("num1 + num2=", num1+num2)
+print("num1 - num2=", num1-num2)
+print("num1 * num2=", num1*num2)
+print("num1 / num2=", num1/num2)
+print("num1 % num2=", num1%num2) #Modulus-Remainder
+print("num1 // num2=", num1//num2) #Floor division-Quotient
+print("num1 ** num2=", num1**num2) #Exponent-Power
+
+#Assignment Operators: =, +=, -=, *=, /=, %=, **=, //=
+#Comparison Operators: ==, !=, >, <, >=, <=
+subs=350
+print(subs==1000)
+if(subs==1000):
+  print("Your channel is eligible for monetization")
+else:
+  print("Your channel is not eligible for monetization")
