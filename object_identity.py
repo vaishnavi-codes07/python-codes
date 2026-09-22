2fc5d2f767bcc99016cc2324e0d31bf8d317e1c9
vaishnavi-codes07 <vaishnavidhonnar2008@gmail.com>
2026-09-22 19:36:54 +0530

    script demonstrating Python object identity and ids.
    Includes a basic example testing id( ) values across integers, lists, and tuples to illustrate how python handles memory allocation and 
    object mutability.

---
+++ object_identity.py
@@ -0,0 +1,16 @@
+a=10
+b=18
+print("id of a=",id(a))
+print("id of b=",id(b))
+print("id of 10=",id(10))
+print("id of 18=",id(18))
+
+list1=[1,2,3]
+list2=[1,2,3]
+print("id of list1=",id(list1))
+print("id of list2=",id(list2))
+
+list1=(1,2,3)
+list2=(1,2,3)
+print("id of list1=",id(list1))
+print("id of list2=",id(list2))
