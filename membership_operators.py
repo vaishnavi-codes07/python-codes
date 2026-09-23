40cd7032647b99264fd40a099906c53b9f7a57d7
vaishnavi-codes07 <vaishnavidhonnar2008@gmail.com>
2026-09-23 19:10:16 +0530

    Python membership operators examples
    Introduces the basic example demonstrating the use of 'in' and 'not in' operators in Python.
    Includes strings, lists, Tuples, and dictionaries.

---
+++ membership_operators.py
@@ -0,0 +1,17 @@
+#Membership operators=in,not in
+str1="Good Morning"
+print("Morning" in str1)
+print("Evening" not in str1)
+
+list1=[1,2,3,4,5]
+print("3" in list1)
+print("6" in list1)
+
+tup1=(1,2,3)
+print("3" in tup1)
+
+dict1={1:"a",2:"b",3:"c"}
+print("1" not in dict1)
+
+set1={1,2,3,4}
+print("3" in set1)
