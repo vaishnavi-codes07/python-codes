ae10cdca63b486c2ba7fa74012df63f839a111ea
vaishnavi-codes07 <vaishnavidhonnar2008@gmail.com>
2026-09-23 19:24:10 +0530

    Python Bitwise operators examples
    Demonstrates the functionality of bitwise operators in Python.
    includes bitwise and, or, xor, not, left shift, right shift.

---
+++ Bitwise_operators.py
@@ -0,0 +1,15 @@
+#Bitwise operators=&, |, ~, ^, >>, <<
+num1=4
+num2=3
+
+print(num1)
+print(num2)
+
+print("num1 & num2=", num1 & num2)
+print("num1 | num2=", num1 | num2)
+print("num1 ^ num2=", num1 ^ num2)
+print("~num1=", ~num1)
+print("~num2=", ~num2)
+
+print("num2<<2=", num2<<2)
+print("num1>>2=", num1>>2)
